3cd4b2fa20bfe882f6b6ea4dcd5f09778cc2670e
Marcelo Nespoli Santos <marcelonespoli@outlook.com>
2020-01-18 21:47:51 +0000

    Added DomainNotificationWithFluentValidator

---
+++ DomainNotificationWithFluentValidator/FluentValidatorExample/FvExample/Domain/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using FvExample.Domain.ValueObjects;
+
+namespace FvExample.Domain.Entities
+{
+    public class Student
+    {
+        public Student(Name name, Document document, Email email)
+        {
+            Name = name;
+            Document = document;
+            Email = email;
+        }
+
+        public Name Name { get; private set; }
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
+    }
+}
---
+++ DomainNotificationWithFluentValidator/FluentValidatorExample/FvExample/Domain/ValueObjects/Name.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidator;
+
+namespace FvExample.Domain.ValueObjects
+{
+    public class Name : Notifiable
+    {
+        public Name(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+            if (FirstName.Length <= 0)
+                AddNotification("FirstName", "Name invalid");
+
+            if (LastName.Length <= 0)
+                AddNotification("LastName", "Lastname invalid");
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
